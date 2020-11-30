fe43f93cc79062fca0047f997ecbd9fdf332f811
Ismael_Barajas <ismaelbarajas30@csu.fullerton.edu>
2020-11-30 00:11:40 -0800

    Got the Icons to show up and stuff

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 import coronaImage from "./images/image.png";
 
+
 class App extends React.Component {
   state = {
     data: {},
     country: "",
   };
+
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
     this.setState({ data: fetchedData, country: country });
   };
+
   render() {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
         <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <br />
-        <text>
           <b>Global and Country Wise Cases of Corona Virus</b>
-        </text>
         <br />
-        <text>
           <i>(For a particular select a Country from below)</i>
-        </text>
         <br />
         <br />
         <Cards data={data} country={country} />
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,30 +1,46 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { api2FetchCountries } from "../../api";
+import { customIcon } from "./customIcon";
+
 
 class MyMap extends React.Component {
   constructor() {
     super();
     this.state = {
-      lat: 38.2,
-      lng: -98.07,
+      lat: 0,
+      lng: 0,
       zoom: 2,
+      data: [],
     };
+  }
+
+  async componentDidMount() {
+    const fetchedData = await api2FetchCountries();
+    this.setState({data: fetchedData});
   }
 
   render() {
     const position = [this.state.lat, this.state.lng];
-
     return (
       <MapContainer
         className="map"
         center={position}
         zoom={this.state.zoom}
-        style={{ height: 500, width: "100%" }}
+        style={{ height: 1000, width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
+
+        {this.state.data.map((countries) => (
+          <Marker
+            key={countries.countryInfo._id}
+            position={[countries.countryInfo.lat, countries.countryInfo.long]}
+            icon={customIcon}
+          ></Marker>
+        ))}
       </MapContainer>
     );
   }
