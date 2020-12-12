c5177c1da8afa58a8bf5b575a7e4ddf5558d2acd
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2020-12-11 18:22:36 -0800

    Removed old code.

--- src/Components/Graphs/Graphs.js
+++ src/Components/Graphs/Graphs.js
@@ -6,30 +6,6 @@ import ScrollUpButton from "react-scroll-up-button";
 const Graphs = () => {
   return (
     <Container maxWidth="xl">
-      {/* <iframe
-              title="Continent filter"
-              src="https://public.domo.com/cards/dPn4z"
-              className={styles.domo1}
-            ></iframe>
-            <iframe
-              title="Key Metrics"
-              src="https://public.domo.com/cards/aOm4g"
-              className={styles.domo1}
-            ></iframe>
-            <iframe
-              title="COVID19 Details"
-              src="https://public.domo.com/cards/dJ45D"
-              className={styles.domo1}
-            ></iframe> */}
-      {/* <iframe
-        title="Dashboard"
-        src="https://public.domo.com/embed/pages/e0lAG?transparentBackground=true"
-        marginheight="0"
-        marginwidth="0"
-        frameborder="0"
-        width="100%"
-        height="2600px"
-      ></iframe> */}
       <div className={styles.divh}>
         <Typography variant="h3">Powered by DOMO</Typography>
       </div>
